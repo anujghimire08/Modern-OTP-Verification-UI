7acbec0902918e655789c0b2ced53b4a776b5a66
Anuj Ghimire <anujghimire08@gmail.com>
2025-11-24 08:26:17 +0545

    Format countdown timer display to include leading zero

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let time = 60;
 
 let timer = setInterval(() => {
   time--;
-  timeEl.textContent = time;
+  timeEl.textContent = `0:${time}`;
   if (time <= 0) {
     clearInterval(timer);
     timetxt.textContent = "you code is expired";
